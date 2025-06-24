5d75cb6a9daf0a4f0a0d1ef216c7bbb5cd28e9c8
indiecastor <vadimtarasov.igrvch@gmail.com>
2025-06-24 17:16:57 +0300

    removed 'randomize' button

--- models/pplv.py
+++ models/pplv.py
@@ -47,27 +47,6 @@ def update(param: Literal['pplv_alpha', 'pplv_beta', 'pplv_gamma', 'pplv_delta',
     dpg.set_value('pred_line', [t_values, solution[: ,1].tolist()])
     dpg.set_value('phase_line', [solution[: ,0].tolist(), solution[: ,1].tolist()])
 
-def randomize() -> None:
-    global pplv_alpha, pplv_beta, pplv_gamma, pplv_delta, pplv_init_prey_pop, pplv_init_pred_pop
-
-    pplv_alpha = round(random.uniform(PPLV_ALPHA_MIN, PPLV_ALPHA_MAX), 3)
-    pplv_beta  = round(random.uniform(PPLV_BETA_MIN, PPLV_BETA_MAX), 3)
-    pplv_gamma = round(random.uniform(PPLV_GAMMA_MIN, PPLV_GAMMA_MAX), 3)
-    pplv_delta = round(random.uniform(PPLV_DELTA_MIN, PPLV_DELTA_MAX), 3)
-
-    pplv_init_prey_pop = round(random.uniform(PPLV_INIT_PREY_POP_MIN, PPLV_INIT_PREY_POP_MAX), 3)
-    pplv_init_pred_pop = round(random.uniform(PPLV_INIT_PREDATOR_POP_MIN, PPLV_INIT_PREDATOR_POP_MAX), 3)
-
-    dpg.configure_item('pplv_alpha', default_value=pplv_alpha)
-    dpg.configure_item('pplv_beta', default_value=pplv_beta)
-    dpg.configure_item('pplv_gamma', default_value=pplv_gamma)
-    dpg.configure_item('pplv_delta', default_value=pplv_delta)
-
-    dpg.configure_item('pplv_init_prey_pop', default_value=pplv_init_prey_pop)
-    dpg.configure_item('pplv_init_pred_pop', default_value=pplv_init_pred_pop)
-
-    update('all')
-
 
 class Window():
     def __init__(self):
@@ -97,8 +76,6 @@ class Window():
                                     min_value=0.1, max_value=10.0, default_value=pplv_init_pred_pop,
                                     width=200, label='Initial predator population',
                                     callback=lambda: update('pplv_init_pred_pop'))
-
-            dpg.add_button(label='Randomize!', callback=randomize)
 
             dpg.add_group(tag='plots_group', horizontal=True)
             with dpg.plot(parent='plots_group', tag='lv_main_plot',
